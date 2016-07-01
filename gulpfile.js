0b690941ed9a3f7f2b4e7b0d7408d1561d584cd3
kungfuboy <17kungfuboy@gmail.com>
2016-07-01 16:20:26 +0800

    new index.html and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
 	gulp.src('src/css/main.scss')
 		.pipe(sass())
+		.pipe(prefix())
 		.pipe(gulp.dest('dist/'));
 });
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: '.'
+        }
+    });
+});
+
+gulp.task('watch', function(){
+    gulp.watch(['*'], ['rebuild']);
+});
+gulp.task('build', ['sass'], function() {
+    gulp.src('*')
+      .pipe(gulp.dest('..'));
+});
+gulp.task('rebuild', ['build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['browserSync', 'watch']);
