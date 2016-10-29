40ff77019f0c451ec6657680d422c40e1cc9cd9f
kungfuboy <17kungfuboy@gmail.com>
2016-10-29 09:42:25 +0800

    include other html

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,16 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var reload      = browserSync.reload;
+var htmlImport  = require('gulp-html-import');
 
-// 静态服务器 + 监听 scss/html 文件
-gulp.task('server', ['sass'], function() {
-
-    browserSync.init({
-        server: "."
-    });
-
-    gulp.watch("src/style/*.scss", ['sass'], reload);
-    gulp.watch("*").on('change', reload);
+// 将所有的文件打包进index.html后实现更新
+gulp.task('import', function() {
+    return gulp.src('src/index.html')
+        .pipe(htmlImport('src/components/'))
+        .pipe(gulp.dest('.'))
+        .pipe(reload({stream: true}));
 });
+
 
 // scss编译后的css将注入到浏览器里实现更新
 gulp.task('sass', function() {
@@ -23,5 +22,19 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("dist/"))
         .pipe(reload({stream: true}));
 });
+
+// 静态服务器 + 监听 scss/html 文件
+gulp.task('server', ['import', 'sass'], function() {
+
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch("src/components/*.html", ['import'], reload);
+    gulp.watch("src/style/*.scss", ['sass'], reload);
+    gulp.watch("*").on('change', reload);
+});
+
+
 
 gulp.task('default', ['server']);
--- src/script/router.js
+++ src/script/router.js
@@ -15,3 +15,26 @@ Router.prototype.init = function() {
 }
 window.Router = new Router();
 window.Router.init();
+
+function changeBg(color, id, m_id) {
+  // change backgroundColor of header and menu color
+  document.querySelector('header').style.backgroundColor = color;
+
+  var w = document.body.clientWidth;
+  if(w < 750) {
+    // mobile
+    Array.prototype.map.call(
+      document.getElementsByClassName("mobile-menu")[0].children, function(n) {
+        n.setAttribute('class', '');
+    });
+    document.getElementById(m_id).setAttribute('class', m_id);
+
+  } else {
+    // pc
+    Array.prototype.map.call(
+      document.getElementsByClassName("pc-menu")[0].children, function(n) {
+        n.setAttribute('class', '');
+    });
+    document.getElementById(id).setAttribute('class', id);
+  }
+}
