44676b9a1d514630bdaa62e13623604ed1e10b56
songjunjie <songjunjie@ygsoft.com>
2018-04-25 15:09:55 +0800

    first

--- vuepress/service-worker.js
+++ vuepress/service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "8f20f3df01461c0e497dbe1881e1dda7"
+    "revision": "dc16dd81abb1350d04e5a86328b5dc7c"
   },
   {
     "url": "assets/css/3.styles.443d84ab.css",
@@ -32,8 +32,8 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.3b5217a8.js",
-    "revision": "7fcbedafe31233afed34a1b087ac5d93"
+    "url": "assets/js/0.a1272708.js",
+    "revision": "16cd8ad80e405e72157e56b0fcc1c050"
   },
   {
     "url": "assets/js/1.c5050887.js",
@@ -44,20 +44,20 @@ self.__precacheManifest = [
     "revision": "d1da5b9f31c17abad8e13e185471ff27"
   },
   {
-    "url": "assets/js/app.d4412a07.js",
-    "revision": "03aa8edd3c54a18581614165c15e1335"
+    "url": "assets/js/app.bcfc2963.js",
+    "revision": "2942ac7867aa00ff61ad0caaa92aa6f6"
   },
   {
     "url": "index.html",
-    "revision": "db85ae5a8a5e139325822f4f6808669d"
+    "revision": "046b643696403053ee9cce22bf1015b5"
   },
   {
     "url": "read/index.html",
-    "revision": "35846264793d0a8f8fad24907299e68f"
+    "revision": "086cdacc917feb7052d7bd31997ae26b"
   },
   {
     "url": "read/read-day.html",
-    "revision": "1e47a531fc449d95efed63ffd23ef1cb"
+    "revision": "913d05c71994ccf8883d9113751839bc"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
