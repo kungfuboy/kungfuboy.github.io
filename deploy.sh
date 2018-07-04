306a0f59afe74194656db9e10b78977431106e13
dark <mac@Mac.local>
2018-07-04 12:31:36 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:kungfuboy/kungfuboy.github.io.git master
+# git push -f git@github.com:kungfuboy/kungfuboy.github.io.git master
+git push -f https://github.com/kungfuboy/kungfuboy.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
